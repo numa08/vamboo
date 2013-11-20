ee1daecf5b7267e06725f78f9e98cb4df868ef4e
numa08 <n511287@gmail.com>
2013-11-20 19:48:51 +0900

    実装の妄想

--- lib/vamboo.rb
+++ lib/vamboo.rb
@@ -1,5 +1,17 @@
 require "vamboo/version"
 
 module Vamboo
-  # Your code goes here...
+
+	def self.default_vamboo_home
+		vamboo_home = if ENV.key?('VAMBOO_HOME')
+	  		ENV['VAMBOO_HOME']
+	  	else
+	  		'/usr/local/etc/vamboo'
+	  	end
+	  	vamboo_home
+	end
+
+	def initialize(vamboo_home)
+		#TODO : create vamboo file
+	end
 end
