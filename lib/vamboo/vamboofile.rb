7b07ef12b11234a5dc85347195b5066e8281e77a
numa08 <n511287@gmail.com>
2013-11-21 18:48:19 +0900

    設定ファイル読み込みクラスを作成

---
+++ lib/vamboo/vamboofile.rb
@@ -0,0 +1,20 @@
+require "domainlist"
+
+class VambooFile
+	Vamboofile = "Vamboofile"
+	attr_reader :vamboo_home
+
+	def self.createAt(vamboo_home)
+	  	FileUtils.mkdir_p(vamboo_home)
+	  	FileUtils.copy("./#{Vamboofile}.org", "#{vamboo_home}/#{Vamboofile}")
+	end
+
+	def initialize(vamboo_home)
+		@vamboo_home = vamboo_home
+	end
+
+	def loadDomains
+		domains = eval(File.read("#{@vamboo_home}/#{Vamboofile}"))
+		domains
+	end
+end
